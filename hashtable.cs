c016df734faafabc15cc4979cb0b5a6e43cccca6
AishwaryaKote28 <100112979+AishwaryaKote28@users.noreply.github.com>
2022-03-09 04:34:12 -0800

    Add files via upload

---
+++ hashtable.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections;
+
+class hashtable
+{
+
+	//static public void Main()
+	//{ 
+	//	hashtable my_hashtable1 = new hashtable();
+	//	my_hashtable1.Add("id", "1");
+	//	my_hashtable1.Add("name", "aishwarya");
+	//	my_hashtable1.Add("ctty", "Solapur");
+
+	//	Console.WriteLine("Key and Value pairs from my_hashtable1:");
+
+	//	foreach (DictionaryEntry ele1 in my_hashtable1)
+	//	{
+	//		Console.WriteLine("{0} and {1} ", ele1.Key, ele1.Value);
+	//	}
+
+		
+	//}
+}
